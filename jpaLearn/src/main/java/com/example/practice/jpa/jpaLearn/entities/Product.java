c422726147098173f8e65baedfe91d1444530c0f
Shikhar Rai <shikharr521@gmail.com>
2025-12-30 19:51:20 +0530

    Added configurations in YAML file

--- jpaLearn/src/main/java/com/example/practice/jpa/jpaLearn/entities/Product.java
+++ jpaLearn/src/main/java/com/example/practice/jpa/jpaLearn/entities/Product.java
@@ -1,13 +1,12 @@
 package com.example.practice.jpa.jpaLearn.entities;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
@@ -17,14 +16,23 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 @Builder
 @Entity
+@Table(name="product_table",
+        uniqueConstraints = {
+                   @UniqueConstraint(name="name_unique",columnNames = {"name"}),
+        }
+)
 public class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(nullable = false,length = 20)
     private String name;
+    @Column(name = "")
     private String title;
     private BigDecimal price;
     private Integer quantity;
+    @CreationTimestamp
     private LocalDateTime createdAt;
+    @UpdateTimestamp
     private LocalDateTime updatedAt;
 }
