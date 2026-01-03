5f04f24951350afa6634436222f3395a75f2e37d
Shikhar Rai <shikharr521@gmail.com>
2026-01-03 15:11:01 +0530

    Tested the Repositories

--- jpaLearn/src/main/java/com/example/practice/jpa/jpaLearn/repositories/ProductRepository.java
+++ jpaLearn/src/main/java/com/example/practice/jpa/jpaLearn/repositories/ProductRepository.java
@@ -2,7 +2,10 @@ package com.example.practice.jpa.jpaLearn.repositories;
 
 import com.example.practice.jpa.jpaLearn.entities.ProductEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 //entity
+@Repository
 public interface ProductRepository extends JpaRepository<ProductEntity,Long> {
 
 }
--- jpaLearn/src/test/java/com/example/practice/jpa/jpaLearn/JpaLearnApplicationTests.java
+++ jpaLearn/src/test/java/com/example/practice/jpa/jpaLearn/JpaLearnApplicationTests.java
@@ -1,13 +1,41 @@
 package com.example.practice.jpa.jpaLearn;
 
+import com.example.practice.jpa.jpaLearn.entities.ProductEntity;
+import com.example.practice.jpa.jpaLearn.repositories.ProductRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.math.BigDecimal;
+import java.util.List;
 
 @SpringBootTest
 class JpaLearnApplicationTests {
 
+    @Autowired
+    ProductRepository productRepository;
+
 	@Test
 	void contextLoads() {
+
 	}
+    @Test
+    void testRepository(){
+        ProductEntity productEntity=ProductEntity.builder()
+                .name("Vlad Von Carstein")
+                .title("VampireLord")
+                .price(BigDecimal.valueOf(2000))
+                .quantity(1)
+                .build();
+
+        ProductEntity savedProductEntity=productRepository.save(productEntity);
+        System.out.println(savedProductEntity);
+    }
+
+    @Test
+    void getRepository(){
+        List<ProductEntity> list=productRepository.findAll();
+        System.out.println(list);
+    }
 
 }
